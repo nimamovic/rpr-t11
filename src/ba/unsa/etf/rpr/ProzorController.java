8b61801bb88833de0e92f6bb5ea31c3e11a9523a
nimamovic <nimamovic3@etf.unsa.ba>
2018-12-26 22:59:23 +0100

    priprema, u gui fali meni za odabir jezika jos

--- src/ba/unsa/etf/rpr/ProzorController.java
+++ src/ba/unsa/etf/rpr/ProzorController.java
@@ -1,17 +1,31 @@
 package ba.unsa.etf.rpr;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import java.sql.Connection;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class ProzorController {
     public TextField gradZaDodavanje;
     public TextField drzavaZaBrisanje;
     public Button dugmeBrisiDrzavu;
     public Label opcija;
+
+
+    public BorderPane mainPane;
+
 
     public void ObrisiDrzavuINjeneGradove(ActionEvent actionEvent) {
         GeografijaDAO gdo = GeografijaDAO.getInstance();
@@ -30,7 +44,7 @@ public class ProzorController {
         }
         else
         {
-            opcija.setText("Dzrazava i njeni gradovi su izbrisani iz baze");
+            opcija.setText("Drzava i njeni gradovi su izbrisani iz baze");
         }
 
     }
@@ -58,5 +72,37 @@ public class ProzorController {
         {
             opcija.setText("Glavni grad "+ gradZaDodavanje.getText()+" je "+gg.getNaziv()+"!");
         }
+    }
+    private void selectLanguage(Locale locale) {
+        Stage primaryStage = (Stage)mainPane.getScene().getWindow();
+        Locale.setDefault(locale);
+        ResourceBundle bundle = ResourceBundle.getBundle("Translation");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("program.fxml"), bundle);
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.setMinWidth(640);
+        primaryStage.setMinHeight(480);
+    }
+
+    public void setBa(ActionEvent actionEvent) {
+        selectLanguage(new Locale("bs","BA"));
+    }
+
+    public void setEn(ActionEvent actionEvent) {
+        selectLanguage(new Locale("en","US"));
+    }
+
+    public void setDe(ActionEvent actionEvent) {
+        selectLanguage(new Locale("de","DE"));
+    }
+
+    public void setFr(ActionEvent actionEvent) {
+        selectLanguage(new Locale("fr","FR"));
     }
 }
